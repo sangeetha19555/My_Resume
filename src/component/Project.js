63dba6fb55a876c15fa538e478e69d45a5938a83
kadir12386 <kadir12386@gmail.com>
2022-05-17 23:01:33 +0300

    2 commit

--- src/component/Project.js
+++ src/component/Project.js
@@ -395,67 +395,6 @@ export default function Project() {
               <div>
                 <img
                   className="project_image"
-                  src="/images/login.png"
-                  alt="Covid_image"
-                />
-              </div>
-
-              <div className="project-box__content">
-                <div className="project-box__content--box">
-                  <div className="project-box__content--box--title">
-                    <h2>Login Page</h2>
-                  </div>
-                  <div className="project-box__content--box--icon">
-                    {/* <GitHubIcon className="icon-git" sx={{ fontSize: 30 }} /> */}
-                    <Tooltip title="Open the Link">
-                      <IconButton>
-                        <a
-                          href="https://reverent-babbage-019cc3.netlify.app/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <OpenInNewIcon
-                            className="icon_color"
-                            sx={{ fontSize: 33 }}
-                          />
-                        </a>
-                      </IconButton>
-                    </Tooltip>
-                  </div>
-                </div>
-                {/* code icon */}
-                <div className="project-box__content--code_icons">
-                  <div className="code_icons bg-orange">
-                    <img
-                      src="images/project_html.png"
-                      alt="html"
-                      width="15px"
-                    />
-                    <span className="text ">Html</span>
-                  </div>
-
-                  <div className="code_icons bg-blue">
-                    <img src="images/project_css.png" alt="css" width="15px" />
-                    <span className="text">Css</span>
-                  </div>
-                </div>
-                {/* ==================== code icon Ends here=======================*/}
-
-                <p className="text_explaination">
-                  This is a Login page UI design using html and Css.
-                </p>
-              </div>
-              {/*=========== Container Box 6 Ends Here ========= */}
-              {/* =========================================================================================== */}
-            </div>
-            {/* ============================================================================ */}
-
-            {/* ============================================================================ */}
-            {/* Container Box 6 starts Here */}
-            <div className="project-box__container">
-              <div>
-                <img
-                  className="project_image"
                   src="/images/kitchen-recipe.png"
                   alt="Covid_image"
                 />
